2ccfc476b3631fbf87586eaef4ada96339724353
fateice <k314874957@outlook.com>
2017-04-11 17:00:06 +0800

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,60 @@
+#include <jni.h>
+#include <string>
+#include <cstdlib>
+#include <math.h>
+#include <ctime>
+
+
+
+extern "C"
+jstring
+Java_com_example_kimi_lbspro_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jdoubleArray JNICALL
+Java_com_example_kimi_lbspro_MainActivity_GridDummy(JNIEnv *env, jobject instance,
+                                                    jdouble mLongitude, jdouble mLatitude, jint k,
+                                                    jdouble s) {
+    // TODO
+    int c = 0;
+
+    //经度
+    double J[100] = {};
+    //纬度
+    double K[100] = {};
+
+    double kk = double(k);
+    c = sqrt(kk);
+    int idx=0,idy=0;
+    srand(unsigned(time(0)));
+    idx = (rand()%(c-1-1))+1;
+    idy = (rand()%(c-1-1))+1;
+    double g = 0;
+    g = sqrt(s)/(c-1);
+    for (int i = 0; i < k; ++i) {
+        K[i] = 0;
+    }
+    for (int i = 0; i < c - 1; ++i) {
+        for (int j = 0; j < c - 1; ++j) {
+            int x = (i-idx)*g + mLongitude;
+            int y = (j-idx)*g + mLatitude;
+            J[j*c+i] = mLongitude;
+            K[j*c+i] = mLatitude;
+        }
+    }
+
+    jdoubleArray result = env->NewDoubleArray(2);
+    double carr[2]={};
+    carr[0]=J[idy*c+idx];
+    carr[1]=K[idy*c+idx];
+    //carr[0]=mLongitude;
+    //carr[1]=mLatitude;
+    env->SetDoubleArrayRegion(result,0,2,carr);
+
+    return result;
+}
---
+++ app/src/main/java/com/example/kimi/lbspro/MainActivity.java
@@ -0,0 +1,487 @@
+package com.example.kimi.lbspro;
+
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "SetLocation";
+    private Thread thread;
+    private MapView mMapView=null;
+    private BaiduMap mBaiduMap;
+    private LocationClient mlocationClient;
+    private MylocationListener mlistener;
+    private Context context;
+
+    private LocationManager mLocationManager;
+    private String mMockProviderName = LocationManager.GPS_PROVIDER;
+
+    //实际经纬度
+    private double mLatitude;
+    private double mLongitude;
+
+    //模拟经纬度
+    private double mmLatitude = 40.052462;
+    private double mmLongitude = 116.29064;
+    private float mCurrentX;
+
+    //计算后的经纬度
+    private double ss[];
+
+    private Button mButton;
+
+    private  TextView mStateBar;
+
+    private TextView cresult;
+
+    boolean siming = false;
+
+    /**
+     * 当前地点击点
+     */
+    private LatLng currentPt;
+    private String touchType;
+
+    private ImageButton mGetMylocationBN;
+
+    //自定义图标
+    private BitmapDescriptor mIconLocation;
+
+    private MyOrientationListener myOrientationListener;
+    //定位图层显示方式
+    private MyLocationConfiguration.LocationMode locationMode;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        this.context=this;
+
+        mStateBar = (TextView)findViewById(R.id.mStateBar);
+        mButton = (Button) findViewById(R.id.button);
+
+
+
+        mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        enableTestProvider();
+        //setLocation();
+
+//        thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try{
+//                    Thread.sleep(500);
+//                }catch(InterruptedException e){
+//                    e.printStackTrace();
+//                }
+//                setLocation();
+//            }
+//        });
+
+        initView();
+        initListener();
+        initLocation();
+
+        //C 返回测试
+        cresult = (TextView)findViewById(R.id.cresult);
+
+    }
+
+    public native String stringFromJNI();
+    public native double[] GridDummy(double mLongitude,double mLatitude,int k,double s);
+
+
+    public void Grid(View view)
+    {
+        ss=GridDummy(mLongitude,mLatitude,100,100);
+        cresult.setText(String.valueOf(ss[0])+","+String.valueOf(ss[1]));
+    }
+
+    public void simGPS(View view){
+        //thread.start();
+        setLocation();
+    }
+
+    public void simClose(View view){
+        //mLocationManager.clearTestProviderEnabled(mMockProviderName);
+        //mLocationManager.removeTestProvider(mMockProviderName);
+        mmLatitude = mLatitude;
+        mmLongitude = mLongitude;
+    }
+
+    private void initView() {
+        mMapView= (MapView) findViewById(R.id.bmapView);
+
+        mBaiduMap=mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+        //根据给定增量缩放地图级别
+        MapStatusUpdate msu= MapStatusUpdateFactory.zoomTo(18.0f);
+        mBaiduMap.setMapStatus(msu);
+        mGetMylocationBN=(ImageButton)findViewById(R.id.id_bn_getMyLocation);
+        mGetMylocationBN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMyLocation();
+            }
+        });
+
+    }
+
+    private void initListener(){
+        mBaiduMap.setOnMapTouchListener(new BaiduMap.OnMapTouchListener() {
+            @Override
+            public void onTouch(MotionEvent motionEvent) {
+
+            }
+        });
+
+
+        mBaiduMap.setOnMapLongClickListener( new BaiduMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng point) {
+                touchType = "长按";
+                currentPt = point;
+                updateMapState();
+            }
+        });
+
+        mBaiduMap.setOnMapStatusChangeListener(new BaiduMap.OnMapStatusChangeListener() {
+            @Override
+            public void onMapStatusChangeStart(MapStatus mapStatus) {
+                updateMapState();
+            }
+
+            @Override
+            public void onMapStatusChange(MapStatus mapStatus) {
+                // updateMapState();
+            }
+
+            @Override
+            public void onMapStatusChangeFinish(MapStatus mapStatus) {
+                updateMapState();
+            }
+        });
+    }
+
+    private void updateMapState(){
+        if(mStateBar == null) {
+            return;
+        }
+        String state = "";
+        if (currentPt == null) {
+            state = "";
+        }else
+        {
+            state = String.format(",当前经度： %f 当前纬度：%f",currentPt.longitude,currentPt.latitude);
+        }
+        state += "\n";
+        mStateBar.setText(state);
+    }
+
+    private void initLocation() {
+        locationMode= MyLocationConfiguration.LocationMode.NORMAL;
+
+        //定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
+        mlocationClient=new LocationClient(this);
+        mlistener=new MylocationListener();
+
+        //注册监听器
+        mlocationClient.registerLocationListener(mlistener);
+        //配置定位SDK各配置参数，比如定位模式、定位时间间隔、坐标系类型等
+        LocationClientOption mOption=new LocationClientOption();
+        //设置坐标类型
+        mOption.setCoorType("bd09ll");
+        //设置是否需要地址信息，默认为无地址
+        mOption.setIsNeedAddress(true);
+        //设置是否打开gps进行定位
+        mOption.setOpenGps(true);
+        //设置扫描间隔，单位是毫秒 当<1000(1s)时，定时定位无效
+        int span=1000;
+        mOption.setScanSpan(span);
+
+        mOption.setEnableSimulateGps(true);
+        //设置 LocationClientOption
+        mlocationClient.setLocOption(mOption);
+
+        //初始化图标,BitmapDescriptorFactory是bitmap 描述信息工厂类.
+        mIconLocation= BitmapDescriptorFactory
+                .fromResource(R.drawable.location_marker);
+
+
+        myOrientationListener=new MyOrientationListener(context);
+        //通过接口回调来实现实时方向的改变
+        myOrientationListener.setOnOrientationListener(new MyOrientationListener.OnOrientationListener() {
+            @Override
+            public void onOrientationChanged(float x) {
+                mCurrentX=x;
+            }
+        });
+
+
+    }
+
+
+
+
+    public void enableTestProvider() {
+        ContentResolver res = context.getContentResolver();
+        //获取GPS状态
+//        boolean gps_enable = Settings.Secure.isLocationProviderEnabled(res, LocationManager.GPS_PROVIDER);
+//        if(gps_enable){
+//            Settings.Secure.setLocationProviderEnabled(res,LocationManager.GPS_PROVIDER,false);
+//        }
+//        //获取允许位置模拟
+//        int mock_enable = Settings.Secure.getInt(res,Settings.Secure.ALLOW_MOCK_LOCATION,0);
+//        if(mock_enable == 0)
+//        {
+//            try{
+//                Settings.Secure.putInt(res,Settings.Secure.ALLOW_MOCK_LOCATION,1);
+//            }catch (Exception e) {
+//                Log.e(TAG,"write error",e);
+//            }
+//        }
+        mLocationManager.addTestProvider(mMockProviderName,
+                "requiresNetwork" == "", "requiresSatellite" == "",
+                "requiresCell" == "", "hasMonetaryCost" == "",
+                "supportsAltitude" == "", "supportsSpeed" == "",
+                "supportsBearing" == "supportsBearing",
+                Criteria.POWER_LOW,
+                Criteria.ACCURACY_FINE
+        );
+        mLocationManager.setTestProviderEnabled(mMockProviderName,true);
+
+    }
+
+    @SuppressLint("NewApi")
+    private void setLocation(){
+        Log.d(TAG,"setLocation");
+        Location loc = new Location(mMockProviderName);
+        loc.setAccuracy(Criteria.ACCURACY_FINE);
+        loc.setTime(System.currentTimeMillis());
+        mmLatitude = currentPt.latitude;
+        mmLongitude = currentPt.longitude;
+        loc.setLatitude(mmLatitude);
+        loc.setLongitude(mmLongitude);
+        loc.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        mLocationManager.setTestProviderLocation(mMockProviderName,loc);
+    }
+
+    public void unenableTestProvider(){
+        int mock_enable = Settings.Secure.getInt(
+                context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION, 0);
+        if(mock_enable == 0) return;
+        try {
+            mLocationManager.clearTestProviderEnabled(mMockProviderName);
+            mLocationManager.removeTestProvider(mMockProviderName);
+        } catch (Exception e) {
+            Log.e(TAG, "", e);
+        }
+//        try {
+//            //关闭 允许模拟地点
+//            Settings.Secure.putInt(context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION, 0);
+//        } catch (Exception e) {
+//            Log.e(TAG, "write error", e);
+//        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if(!mlocationClient.isStarted())
+        {
+            mlocationClient.start();
+        }
+        myOrientationListener.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //停止定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mlocationClient.stop();
+        myOrientationListener.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unenableTestProvider();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void getMyLocation()
+    {
+        LatLng latLng=new LatLng(mLatitude,mLongitude);
+        MapStatusUpdate msu= MapStatusUpdateFactory.newLatLng(latLng);
+        mBaiduMap.setMapStatus(msu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.id_map_common:
+                mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.id_map_site:
+                mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.id_map_traffic:
+                if(mBaiduMap.isTrafficEnabled())
+                {
+                    mBaiduMap.setTrafficEnabled(false);
+                    item.setTitle("实时交通(off)");
+                }else
+                {
+                    mBaiduMap.setTrafficEnabled(true);
+                    item.setTitle("实时交通(on)");
+                }
+                break;
+            case R.id.id_map_mlocation:
+                getMyLocation();
+                break;
+            case R.id.id_map_model_common:
+                //普通模式
+                locationMode= MyLocationConfiguration.LocationMode.NORMAL;
+                break;
+            case R.id.id_map_model_following:
+                //跟随模式
+                locationMode= MyLocationConfiguration.LocationMode.FOLLOWING;
+                break;
+            case R.id.id_map_model_compass:
+                //罗盘模式
+                locationMode= MyLocationConfiguration.LocationMode.COMPASS;
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //所有的定位信息都通过接口回调来实现
+    public class MylocationListener implements BDLocationListener
+    {
+        //定位请求回调接口
+        private boolean isFirstIn=true;
+        //定位请求回调函数,这里面会得到定位信息
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            //BDLocation 回调的百度坐标类，内部封装了如经纬度、半径等属性信息
+            //MyLocationData 定位数据,定位数据建造器
+            /*
+            * 可以通过BDLocation配置如下参数
+            * 1.accuracy 定位精度
+            * 2.latitude 百度纬度坐标
+            * 3.longitude 百度经度坐标
+            * 4.satellitesNum GPS定位时卫星数目 getSatelliteNumber() gps定位结果时，获取gps锁定用的卫星数
+            * 5.speed GPS定位时速度 getSpeed()获取速度，仅gps定位结果时有速度信息，单位公里/小时，默认值0.0f
+            * 6.direction GPS定位时方向角度
+            * */
+            mLatitude = bdLocation.getLatitude();
+            mLongitude=bdLocation.getLongitude();
+
+            MyLocationData data= new MyLocationData.Builder()
+                    .direction(mCurrentX)//设定图标方向
+                    .accuracy(bdLocation.getRadius())//getRadius 获取定位精度,默认值0.0f
+                    .latitude(mLatitude)//百度纬度坐标
+                    .longitude(mLongitude)//百度经度坐标
+                    .build();
+            //设置定位数据, 只有先允许定位图层后设置数据才会生效，参见 setMyLocationEnabled(boolean)
+            mBaiduMap.setMyLocationData(data);
+            //配置定位图层显示方式,三个参数的构造器
+            /*
+            * 1.定位图层显示模式
+            * 2.是否允许显示方向信息
+            * 3.用户自定义定位图标
+            *
+            * */
+            MyLocationConfiguration configuration
+                    =new MyLocationConfiguration(locationMode,true,mIconLocation);
+            //设置定位图层配置信息，只有先允许定位图层后设置定位图层配置信息才会生效，参见 setMyLocationEnabled(boolean)
+            mBaiduMap.setMyLocationConfigeration(configuration);
+            //判断是否为第一次定位,是的话需要定位到用户当前位置
+            if(isFirstIn)
+            {
+                //地理坐标基本数据结构
+                LatLng latLng=new LatLng(bdLocation.getLatitude(),bdLocation.getLongitude());
+                //描述地图状态将要发生的变化,通过当前经纬度来使地图显示到该位置
+                MapStatusUpdate msu= MapStatusUpdateFactory.newLatLng(latLng);
+                //改变地图状态
+                mBaiduMap.setMapStatus(msu);
+                isFirstIn=false;
+                Toast.makeText(context, bdLocation.getAddrStr(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+
+        }
+    }
+
+}
+
