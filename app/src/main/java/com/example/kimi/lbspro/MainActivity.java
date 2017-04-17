e78ab5f45cddcdb626f263815bbc120e8a3516e6
fateice <k314874957@outlook.com>
2017-04-17 16:52:35 +0800

    clear

--- app/src/main/java/com/example/kimi/lbspro/MainActivity.java
+++ app/src/main/java/com/example/kimi/lbspro/MainActivity.java
@@ -165,6 +165,7 @@ public class MainActivity extends AppCompatActivity {
         //mLocationManager.removeTestProvider(mMockProviderName);
         mmLatitude = mLatitude;
         mmLongitude = mLongitude;
+        mBaiduMap.clear();
     }
 
     private void initView() {
